e63e1e73b491e44af2f65bc8c34fad4286a4c7e5
Birch <birch17261@gmail.com>
2024-03-13 14:36:56 +0800

    chore(omit): db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "better-sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["www_*"],
-} satisfies Config;
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   driver: "better-sqlite",
+//   dbCredentials: {
+//     url: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["www_*"],
+// } satisfies Config;
